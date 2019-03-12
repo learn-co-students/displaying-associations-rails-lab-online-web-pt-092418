407dd19a0b8f1d655d0d13cc81f02f83e21dc4a1
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-03-12 10:28:53 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108015408) do
+ActiveRecord::Schema.define(version: 20190311161257) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "songs_id"
   end
+
+  add_index "artists", ["songs_id"], name: "index_artists_on_songs_id"
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "artist_id"
   end
+
+  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id"
 
 end
